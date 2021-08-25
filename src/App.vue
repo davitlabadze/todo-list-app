ca8eabd459899b2b56b712cb37dce6e7cb002099
davitlabadze <labadzedato18@gmail.com>
2021-08-25 17:42:56 +0400

    todo-app

---
+++ src/App.vue
@@ -0,0 +1,124 @@
+<template>
+<div class="container">
+  <Header title="ToDo App" />
+  <AddTask @add-task="addTask" :showAddTask="showAddTask"/>
+ 
+  <hr>
+  <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
+
+</div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks,
+    AddTask
+  },
+  data(){
+    return{
+      tasks: [],
+      showAddTask: false
+    }
+  },
+  methods: {
+    // toggleAddTask(){
+    //   this.showAddTask = !this.showAddTask
+    // },
+    addTask(task){
+      this.tasks = [...this.tasks, task]
+    },
+    deleteTask(id){
+      if(confirm('Are you sure?')){
+        this.tasks = this.tasks.filter((task)=> task.id !== id)
+      }
+    },
+    toggleReminder(id){
+      this.tasks = this.tasks.map((task)=> 
+      task.id == id?{...task, reminder: !task
+      .reminder}: task)
+    }
+  },
+  created(){
+    this.tasks = [
+      {
+        id: 1,
+        text: 'doctors Appointment',
+        value: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+       {
+        id: 2,
+        text: 'Meeting Appointment',
+        value: 'March 1st at 2:30pm',
+        reminder: false,
+      },
+       {
+        id: 3,
+        text: 'School Appointment',
+        value: 'March 1st at 2:30pm',
+        reminder: false,
+      },
+    ]
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+*{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body{
+  font-family: 'Poppins', sans-serif;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+/* .btn{
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+} */
+.btn:focus{
+  outline: none;
+}
+.btn:active{
+  transform: scale(0.98);
+}
+.btn-block{
+  display:block;
+  width: 100%;
+}
+</style>
